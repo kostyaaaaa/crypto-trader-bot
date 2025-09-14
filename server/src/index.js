f753edac135c087188c40db32517184230f47951
Kostia <kostyaarabadji@gmail.com>
2025-09-14 18:25:30 +0300

    fix: :bug: add cors

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import globalRouter from './routes/index.js';
 import logger from './utils/Logger.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -9,10 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Use global router
-app.use('/api', globalRouter);
+app.use('/', globalRouter);
 
 // Start server
 app.listen(PORT, () => {
