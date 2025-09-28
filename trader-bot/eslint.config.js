421ee5581f5029be5180834bf98263f961f96d4f
Kostia <kostyaarabadji@gmail.com>
2025-09-28 15:23:02 +0300

    feat(app): :sparkles: add eslint to all workspaces

---
+++ trader-bot/eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    files: ['src/**/*.js'],
+    ...js.configs.recommended,
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.es2022,
+      },
+    },
+    rules: {
+      // Customize rules for JavaScript
+      'no-unused-vars': 'warn',
+      'no-console': 'off', // Allow console.log in this project
+      'no-undef': 'warn', // Make undefined variables warnings instead of errors
+    },
+  },
+  {
+    files: ['eslint.config.js'],
+    ...js.configs.recommended,
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: globals.node,
+    },
+  },
+];
